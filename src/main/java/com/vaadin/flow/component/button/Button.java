10d08b94598a2a1b2ef6f5cda8f8bcf5e0000dc1
Matti Tahvonen <matti@vaadin.com>
2018-12-10 17:46:34 +0200

    updated PR according to requests

--- src/main/java/com/vaadin/flow/component/button/Button.java
+++ src/main/java/com/vaadin/flow/component/button/Button.java
@@ -319,28 +319,21 @@ public class Button extends GeneratedVaadinButton<Button>
     }
 
     /**
-     * Executes a click on this button at the client-side. Calling this method
-     * behaves exactly the same as if the user would have clicked on the button.
+     * Simulates a click on this button on the server side. Calling this method
+     * executes all registered click listeners on the server side, but does not 
+     * execute possible client side registered listeners.
      * 
-     * @deprecated use {@link #fireClick()} or {@link #fireClickInClient()} instead.
+     * @see #clickInClient() 
      */
-    @Deprecated
     public void click() {
         fireEvent(new ClickEvent<>(this, false, 0,0,0,0,0,0,false,false,false,false));
     }
     
     /**
-     * Executes all click listeners directly on the server side.
-     */
-    public void fireClick() {
-        fireEvent(new ClickEvent<>(this, false, 0,0,0,0,0,0,false,false,false,false));
-    }
-
-    /**
      * Executes a click on this button at the client-side. Calling this method
      * behaves the same as if the user would have clicked on the button.
      */
-    public void fireClickInClient() {
+    public void clickInClient() {
         getElement().callFunction("click");
     }
 
